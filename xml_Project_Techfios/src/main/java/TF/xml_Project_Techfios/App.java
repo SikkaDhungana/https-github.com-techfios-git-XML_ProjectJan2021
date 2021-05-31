5ad711cae338e22cdbc429f24ce469155638b011
SikkaDhungana <sikkadhungana@gmail.com>
2021-05-31 10:12:48 -0400

    First Commit

---
+++ xml_Project_Techfios/src/main/java/TF/xml_Project_Techfios/App.java
@@ -0,0 +1,13 @@
+package TF.xml_Project_Techfios;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
