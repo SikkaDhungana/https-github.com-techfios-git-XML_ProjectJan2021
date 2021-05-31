d64d864a35482898fd2f40c281983ef6a6323043
SikkaDhungana <sikkadhungana@gmail.com>
2021-05-31 10:36:26 -0400

    First Commit

---
+++ src/test/java/test/AddCustomerTest.java
@@ -0,0 +1,71 @@
+package test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import page.AddCustomerPage;
+import page.DashboardPage;
+import page.LoginPage;
+import util.BrowserFactory;
+//import util.ExcelReader;
+
+public class AddCustomerTest {
+
+	WebDriver driver;
+	String userName;
+	String Password;
+
+	String fullName;
+	String companyName;
+	String email;
+	String phone;
+	String address;
+	String city;
+	String country;
+	String state;
+	String zip;
+
+	@Test
+	@Parameters({ "userName", "password", "FullName", "CompanyName", "Email", "Phone", "Address", "City", "Country",
+			"State", "Zip" })
+	public void validUserShouldBeAbleToCreateCustomer(String userName, String password, String fullName,
+			String companyName, String email, String phone, String address, String city, String country, String state,
+			String zip) {
+
+		driver = BrowserFactory.init();
+
+		LoginPage loginPage = PageFactory.initElements(driver, LoginPage.class);
+		loginPage.enterUserName(userName);
+		loginPage.enterPassword(password);
+		loginPage.clickSigninButton();
+
+		DashboardPage dashboardPage = PageFactory.initElements(driver, DashboardPage.class);
+		// dashboardPage.verifyDashboardPage();
+		dashboardPage.clickCustomersButton();
+		dashboardPage.clickAddCustomersButton();
+
+		AddCustomerPage addCustomerPage = PageFactory.initElements(driver, AddCustomerPage.class);
+		addCustomerPage.enterFullName(fullName);
+		addCustomerPage.enterCompany(companyName);
+		addCustomerPage.enterEmail(email);
+		addCustomerPage.enterPhone(phone);
+		addCustomerPage.enterAddress(address);
+		addCustomerPage.enterCity(city);
+		addCustomerPage.enterState(state);
+		addCustomerPage.enterZip(zip);
+		addCustomerPage.enterCountry(country);
+		addCustomerPage.clickSaveButtonOnAddContact();
+
+		addCustomerPage.verifySummaryPage();
+		dashboardPage.clickListCustomersButton();
+
+		addCustomerPage.verifyEnteredNameAndDelete();
+		addCustomerPage.deleteConformOk();
+
+		// BrowserFactory.tearDown();
+
+	}
+
+}
